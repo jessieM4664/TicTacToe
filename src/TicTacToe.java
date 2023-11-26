1596ebfb55b8dc7a0f0d66975a736aa9fa793253
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-11-26 17:12:54 -0500

    methods created

---
+++ src/TicTacToe.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class TicTacToe implements ActionListener {
+    
+    TicTacToe() {
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void xWins(int a, int b, int c) {
+
+    }
+    
+    public void oWins(int a, int b, int c) {
+
+    }
+
+
+}
